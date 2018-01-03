bc34e33249ce88f249f8be64f3fc7719e872dbc7
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-03 19:21:41 +0800

    updated back button

--- src/android/InAppBrowserDialog.java
+++ src/android/InAppBrowserDialog.java
@@ -46,7 +46,7 @@ public class InAppBrowserDialog extends Dialog {
 
     public void onBackPressed () {
       //LOG.d(LOG_TAG, "back button pressed");         
-      Toast.makeText(this.cordova.getActivity(),"back button pressed",Toast.LENGTH_LONG).show();
+      Toast.makeText(this.cordova.getActivity().cordova.getActivity(),"back button pressed",Toast.LENGTH_LONG).show();
 
         if (this.inAppBrowser == null) {
             this.dismiss();
