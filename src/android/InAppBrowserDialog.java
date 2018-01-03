ddef20260403c1a875e0db87f1766d73c5daadd5
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-03 10:00:51 +0800

    testing with back button

--- src/android/InAppBrowserDialog.java
+++ src/android/InAppBrowserDialog.java
@@ -20,10 +20,47 @@ package org.apache.cordova.inappbrowser;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpStack;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.eghl.android.sdk.R;
+import com.eghl.sdk.interfaces.CaptureCallback;
+import com.eghl.sdk.interfaces.MasterpassCallback;
+import com.eghl.sdk.interfaces.QueryCallback;
+import com.eghl.sdk.masterpass.MasterPassActivity;
+import com.eghl.sdk.params.CaptureParams;
+import com.eghl.sdk.params.MasterpassParams;
+import com.eghl.sdk.params.Params;
+import com.eghl.sdk.params.PaymentParams;
+import com.eghl.sdk.params.QueryParams;
+import com.eghl.sdk.payment.PaymentActivity;
+import com.eghl.sdk.response.CaptureResponse;
+import com.eghl.sdk.response.QueryResponse;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 /**
  * Created by Oliver on 22/11/2013.
