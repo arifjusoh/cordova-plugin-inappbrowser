59f67287fb04831203b32fe2fdd5ba7a26d52bad
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-03 15:04:56 +0800

    updated back button

--- src/android/InAppBrowserDialog.java
+++ src/android/InAppBrowserDialog.java
@@ -25,19 +25,14 @@ import android.content.Context;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 /**
  * Created by Oliver on 22/11/2013.
  */
 public class InAppBrowserDialog extends Dialog {
     Context context;
     InAppBrowser inAppBrowser = null;
+
+    import android.content.DialogInterface; //in InAppBrowserDialog class
 
 
 
