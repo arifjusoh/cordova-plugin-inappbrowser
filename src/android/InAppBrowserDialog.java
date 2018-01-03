3bb2000bdfbc647d3ec8a929c96c54b6e1137e61
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-03 15:02:00 +0800

    updated back button

--- src/android/InAppBrowserDialog.java
+++ src/android/InAppBrowserDialog.java
@@ -28,8 +28,6 @@ import org.json.JSONObject;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.DialogInterface;
-import android.view.View.OnClickListener;
-import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
