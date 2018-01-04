b22ec3d5f6297d20eabf8742fe5a414c5722b617
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-04 19:24:12 +0800

    updated close button

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -466,7 +466,13 @@ else
 	Toast.makeText(this.cordova.getActivity(),"not allowed to close",Toast.LENGTH_LONG).show();
 
 			this.cordova.getActivity().runOnUiThread(new Runnable() {
-           
+            @Override
+            public void run() {
+                // NB: From SDK 19: "If you call methods on WebView from any thread
+                // other than your app's UI thread, it can cause unexpected results."
+                // http://developer.android.com/guide/webapps/migrating.html#Threads
+                //childView.loadUrl("about:blank");
+            //Toast.makeText(this.cordova.getActivity(),"going to try",Toast.LENGTH_LONG).show();
                 try {
                     JSONObject obj = new JSONObject();
                     obj.put("type", EXIT_EVENT);
@@ -474,7 +480,7 @@ else
                 } catch (JSONException ex) {
                     LOG.d(LOG_TAG, "Should never happen");
                 }
-            
+            }
         });
 
 }
