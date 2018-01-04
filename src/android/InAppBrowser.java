7580de0d5f34b06f9060c9376dcedefe52bc7490
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-04 20:13:41 +0800

    updated close button

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -424,82 +424,47 @@ public class InAppBrowser extends CordovaPlugin {
 
     	Toast.makeText(this.cordova.getActivity(),"shouldClose: "+String.valueOf(shouldClose),Toast.LENGTH_LONG).show();
 
-    	if(String.valueOf(shouldClose).equals("true"))
-    	{
-        this.cordova.getActivity().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                final WebView childView = inAppWebView;
+    	
+    	this.cordova.getActivity().runOnUiThread(new Runnable() {
+    		@Override
+    		public void run() {
+    			final WebView childView = inAppWebView;
                 // The JS protects against multiple calls, so this should happen only when
                 // closeDialog() is called by other native code.
-                if (childView == null) {
-                    return;
-                }
+    			if (childView == null) {
+    				return;
+    			}
 
-                childView.setWebViewClient(new WebViewClient() {
+    			childView.setWebViewClient(new WebViewClient() {
                     // NB: wait for about:blank before dismissing
-                    public void onPageFinished(WebView view, String url) {
-                        if (dialog != null) {
-                            dialog.dismiss();
-                            dialog = null;
-                        }
-                    }
-                });
+    				public void onPageFinished(WebView view, String url) {
+    					if (dialog != null) {
+    						dialog.dismiss();
+    						dialog = null;
+    					}
+    				}
+    			});
                 // NB: From SDK 19: "If you call methods on WebView from any thread
                 // other than your app's UI thread, it can cause unexpected results."
                 // http://developer.android.com/guide/webapps/migrating.html#Threads
-                childView.loadUrl("about:blank");
+    			childView.loadUrl("about:blank");
 
-                try {
-                    JSONObject obj = new JSONObject();
-                    obj.put("type", EXIT_EVENT);
-                    //sendUpdate(obj, false);
-                } catch (JSONException ex) {
-                    LOG.d(LOG_TAG, "Should never happen");
-                }
-            }
-        });
-    }
+    			try {
+    				JSONObject obj = new JSONObject();
+    				obj.put("type", EXIT_EVENT);
+    				if (!shouldClose) {
+    					sendUpdate(obj, false);
+    				}
 
-else
-{
-	Toast.makeText(this.cordova.getActivity(),"not allowed to close",Toast.LENGTH_LONG).show();
 
-			this.cordova.getActivity().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                final WebView childView = inAppWebView;
-                // The JS protects against multiple calls, so this should happen only when
-                // closeDialog() is called by other native code.
-                if (childView == null) {
-                    return;
-                }
+    			} catch (JSONException ex) {
+    				Toast.makeText(this.cordova.getActivity(),"exception:"+ex),Toast.LENGTH_LONG).show();
 
-                childView.setWebViewClient(new WebViewClient() {
-                    // NB: wait for about:blank before dismissing
-                    public void onPageFinished(WebView view, String url) {
-                        if (dialog != null) {
-                            //dialog.dismiss();
-                            //dialog = null;
-                        }
-                    }
-                });
-                // NB: From SDK 19: "If you call methods on WebView from any thread
-                // other than your app's UI thread, it can cause unexpected results."
-                // http://developer.android.com/guide/webapps/migrating.html#Threads
-                //childView.loadUrl("about:blank");
-            //Toast.makeText(this.cordova.getActivity(),"going to try",Toast.LENGTH_LONG).show();
-                try {
-                    JSONObject obj = new JSONObject();
-                    obj.put("type", EXIT_EVENT);
-                    sendUpdate(obj, false);
-                } catch (JSONException ex) {
-                    LOG.d(LOG_TAG, "Should never happen");
-                }
-            }
-        });
+    				LOG.d(LOG_TAG, "Should never happen");
+    			}
+    		}
+    	});
 
-}
 
 }
 
