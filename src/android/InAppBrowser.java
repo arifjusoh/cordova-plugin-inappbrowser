2ea316f162ba80b7f141bafad5a91c776d27cbd5
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-05 00:00:17 +0800

    updated back button

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -436,7 +436,7 @@ public class InAppBrowser extends CordovaPlugin {
                     // NB: wait for about:blank before dismissing
                     public void onPageFinished(WebView view, String url) {
                         if (dialog != null) {
-                        	 if (shouldClose) {
+                        	 if (String.valueOf(shouldClose) == "yes") {
                             dialog.dismiss();
                             dialog = null;
                         }
@@ -446,7 +446,7 @@ public class InAppBrowser extends CordovaPlugin {
                 // NB: From SDK 19: "If you call methods on WebView from any thread
                 // other than your app's UI thread, it can cause unexpected results."
                 // http://developer.android.com/guide/webapps/migrating.html#Threads
-                 if (shouldClose) {
+                 if (String.valueOf(shouldClose) == "yes") {
                 childView.loadUrl("about:blank");
 				}
                 // try {
@@ -460,7 +460,7 @@ public class InAppBrowser extends CordovaPlugin {
                  try {
     				JSONObject obj = new JSONObject();
     				obj.put("type", EXIT_EVENT);
-    				if (!shouldClose) {
+    				 if (String.valueOf(shouldClose) == "no") {
     					sendUpdate(obj, false);
     				}
 
@@ -583,7 +583,7 @@ public class InAppBrowser extends CordovaPlugin {
             if (shouldclose != null) {
                 shouldClose = shouldclose.booleanValue();
 
-                //Toast.makeText(this.cordova.getActivity(),String.valueOf(shouldClose),Toast.LENGTH_LONG).show();
+                Toast.makeText(this.cordova.getActivity(),String.valueOf(shouldClose),Toast.LENGTH_LONG).show();
             }
             Boolean zoom = features.get(ZOOM);
             if (zoom != null) {
