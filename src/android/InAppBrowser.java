20c89350bdcdea1c951390a33f74570ed4e9daf4
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-05 09:47:04 +0800

    updated back button

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -524,7 +524,7 @@ Toast.makeText(this.cordova.getActivity(),"close func",Toast.LENGTH_LONG).show()
      * @param url to load
      */
     private void navigate(String url) {
-    	Toast.makeText(this.cordova.getActivity(),url,Toast.LENGTH_LONG).show();
+    	Toast.makeText(this.cordova.getActivity(),"hardware back",Toast.LENGTH_LONG).show();
         InputMethodManager imm = (InputMethodManager)this.cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(edittext.getWindowToken(), 0);
 
@@ -767,7 +767,6 @@ Toast.makeText(this.cordova.getActivity(),"close func",Toast.LENGTH_LONG).show()
 
                 // WebView
                 inAppWebView = new WebView(cordova.getActivity());
-                Toast.makeText(this.cordova.getActivity(),"webview",Toast.LENGTH_LONG).show();
                 inAppWebView.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
                 inAppWebView.setId(Integer.valueOf(6));
                 // File Chooser Implemented ChromeClient
@@ -814,7 +813,6 @@ Toast.makeText(this.cordova.getActivity(),"close func",Toast.LENGTH_LONG).show()
 
                 });
                 WebViewClient client = new InAppBrowserClient(thatWebView, edittext);
-                 Toast.makeText(this.cordova.getActivity(),"webview client",Toast.LENGTH_LONG).show();
                 inAppWebView.setWebViewClient(client);
                 WebSettings settings = inAppWebView.getSettings();
                 settings.setJavaScriptEnabled(true);
@@ -857,7 +855,6 @@ Toast.makeText(this.cordova.getActivity(),"close func",Toast.LENGTH_LONG).show()
                     CookieManager.getInstance().setAcceptThirdPartyCookies(inAppWebView,true);
                 }
 
- 				Toast.makeText(this.cordova.getActivity(),"inAppWebView loading",Toast.LENGTH_LONG).show();
                 inAppWebView.loadUrl(url);
                 inAppWebView.setId(Integer.valueOf(6));
                 inAppWebView.getSettings().setLoadWithOverviewMode(true);
