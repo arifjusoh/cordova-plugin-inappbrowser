bb382596113934abe82447be1e5929e00e6a7680
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-07 01:08:57 +0800

    updated

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -79,7 +79,7 @@ public class InAppBrowser extends CordovaPlugin {
     private static final String SYSTEM = "_system";
     private static final String EXIT_EVENT = "exit";
     private static final String LOCATION = "location";
-    private static final String SHOULD_CLOSE = "shouldclose";
+    public static final String SHOULD_CLOSE = "shouldclose";
     private static final String ZOOM = "zoom";
     private static final String HIDDEN = "hidden";
     private static final String LOAD_START_EVENT = "loadstart";
@@ -98,7 +98,7 @@ public class InAppBrowser extends CordovaPlugin {
     private EditText edittext;
     private CallbackContext callbackContext;
     private boolean showLocationBar = true;
-    private boolean shouldClose = true; //default true means can close, unless specified otherwise
+    public boolean shouldClose = true; //default true means can close, unless specified otherwise
     private boolean showZoomControls = true;
     private boolean openWindowHidden = false;
     private boolean clearAllCache = false;
