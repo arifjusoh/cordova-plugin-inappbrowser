d237fb184e5a15767d2a1fbb1bb74802bff3cfce
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-06 23:04:59 +0800

    updated

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -52,9 +52,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
- import android.content.DialogInterface;
- import android.app.AlertDialog;
-import android.app.Dialog;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.Config;
@@ -497,36 +494,11 @@ public class InAppBrowser extends CordovaPlugin {
      */
     public void goBack() {
 
-        // if (this.inAppWebView.canGoBack()) {
-        //     this.inAppWebView.goBack();
-        // }
+        if (this.inAppWebView.canGoBack()) {
+            this.inAppWebView.goBack();
+        }
 
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context)
-        .setTitle("Exit")
-        .setMessage("You are about to exit, are you sure?")
-        .setPositiveButton("Exit", new DialogInterface.OnClickListener(){
-            public void onClick(DialogInterface dialog, int which){
-                if (this.inAppWebView == null) {
-                    dismiss();
-                } 
-                else {
-                    // better to go through the in inAppBrowser
-                    // because it does a clean up
-                    if (this.inAppWebView.canGoBack()) {
-                       this.inAppWebView.goBack();
-                    }  else {
-                       this.inAppWebView.closeDialog();
-                    }
-                }
-            }
-        })
-        .setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
-            public void onClick(DialogInterface dialog,int which){
-                dialog.cancel();
-            }
-        });
-        alertDialogBuilder.create();
-        alertDialogBuilder.show();
+        
 
     }
 
