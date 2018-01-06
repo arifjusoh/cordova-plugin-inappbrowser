0f8fd2fb578ad22aaaa9d5aa583f530ef48c0751
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-07 00:19:42 +0800

    updated

--- src/android/InAppBrowserDialog.java
+++ src/android/InAppBrowserDialog.java
@@ -44,7 +44,24 @@ public class InAppBrowserDialog extends Dialog {
     }
 
     public void onBackPressed() {
-    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context)
+      if (shouldClose)
+      {
+        if (this.inAppBrowser == null) {
+            this.dismiss();
+        } else {
+            // better to go through the in inAppBrowser
+            // because it does a clean up
+            if (this.inAppBrowser.hardwareBack() && this.inAppBrowser.canGoBack()) {
+                this.inAppBrowser.goBack();
+            }  else {
+                this.inAppBrowser.closeDialog();
+            }
+        }
+      }
+
+      else
+      {
+      AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context)
         .setTitle("Exit")
         .setMessage("You are about to exit, are you sure?")
         .setPositiveButton("Exit", new DialogInterface.OnClickListener(){
@@ -70,5 +87,6 @@ public class InAppBrowserDialog extends Dialog {
         });
         alertDialogBuilder.create();
         alertDialogBuilder.show();
+   }
 }
 }
