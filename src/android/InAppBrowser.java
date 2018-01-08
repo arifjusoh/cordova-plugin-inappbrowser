452a2a7bef66bbba389f88b299cefda4c1ab6881
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-08 16:33:32 +0800

    updated

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -731,8 +731,8 @@ public class InAppBrowser extends CordovaPlugin {
 
                 close.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
-                       closeDialog();
-
+                       //closeDialog();
+						this.inAppWebView.canGoBack();
                     	//InAppBrowser.hardwareBack();
                     }
                 });
