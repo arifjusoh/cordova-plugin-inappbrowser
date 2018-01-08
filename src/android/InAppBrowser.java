80d23b37f425803f669a52adeb45290eecd47fcf
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-08 19:18:05 +0800

    updated

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -425,42 +425,7 @@ public class InAppBrowser extends CordovaPlugin {
      */
     public void closeDialog() {
 
-    	if(shouldClose)
-      {
-         //closeDialog();
-      	alert("normal close");
-      }
 
-    else
-      {
-    // AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context)
-    //     .setTitle("Are you sure you want to quit")
-    //     .setMessage("Pressing EXIT button will close and abandon the payment session")
-    //     .setPositiveButton("EXIT", new DialogInterface.OnClickListener(){
-    //         public void onClick(DialogInterface dialog, int which){
-    //             if (inAppBrowser == null) {
-    //                 dismiss();
-    //             } 
-    //             else {
-    //                 // better to go through the in inAppBrowser
-    //                 // because it does a clean up
-    //                 if (hardwareBack() && canGoBack()) {
-    //                     goBack();
-    //                 }  else {
-    //                     //closeDialog();
-    //                     alert("normal close");
-    //                 }
-    //             }
-    //         }
-    //     })
-    //     .setNegativeButton("CANCEL", new DialogInterface.OnClickListener(){
-    //         public void onClick(DialogInterface dialog,int which){
-    //             dialog.cancel();
-    //         }
-    //     });
-    //     alertDialogBuilder.create();
-    //     alertDialogBuilder.show();
-      }
 
 /*
         this.cordova.getActivity().runOnUiThread(new Runnable() {
