5afa7730b6eff04066bed894f6fa6d3ab7b3b622
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-08 10:27:06 +0800

    updated

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -731,7 +731,7 @@ public class InAppBrowser extends CordovaPlugin {
 
                 close.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
-                    	 Toast.makeText(this.cordova.getActivity(),"toolbar close pressed",Toast.LENGTH_SHORT).show();
+                    	 //Toast.makeText(this.cordova.getActivity(),"toolbar close pressed",Toast.LENGTH_SHORT).show();
                        closeDialog();
                     }
                 });
