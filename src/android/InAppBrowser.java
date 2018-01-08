bc8d9d2071918edc3ea459b6c51afcdc14fd8cd4
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-08 18:18:16 +0800

    updated

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -732,6 +732,7 @@ public class InAppBrowser extends CordovaPlugin {
                 close.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
                        closeDialog();
+                        finish ();
 						}
                 });
 
