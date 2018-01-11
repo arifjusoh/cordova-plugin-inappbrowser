a8df631b4fa8574b7ad7943a4cd708a43c88566e
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-11 11:33:15 +0800

    updated

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1051,34 +1051,34 @@ public class InAppBrowser extends CordovaPlugin {
             return false;
         }
 
-        private String convertQueryToJSON(Uri uri){ 
-        try{
-            Set<String> names = uri.getQueryParameterNames();
-            JSONObject json = new JSONObject();
+    //     private String convertQueryToJSON(Uri uri){ 
+    //     try{
+    //         Set<String> names = uri.getQueryParameterNames();
+    //         JSONObject json = new JSONObject();
 
-            for(String name: names){
-                String value = uri.getQueryParameter(name) != null? uri.getQueryParameter(name): "";
-                json.put(name,value);
+    //         for(String name: names){
+    //             String value = uri.getQueryParameter(name) != null? uri.getQueryParameter(name): "";
+    //             json.put(name,value);
 
-            }
-            return json.toString();
+    //         }
+    //         return json.toString();
 
-        }catch(Exception e){
-            //ELogger.e(TAG,"Error converting to json",e);
-        	Toast.makeText(this.cordova.getActivity(),"Error converting to json",Toast.LENGTH_SHORT).show();
-            return "";
-        }
+    //     }catch(Exception e){
+    //         //ELogger.e(TAG,"Error converting to json",e);
+    //     	Toast.makeText(this.cordova.getActivity(),"Error converting to json",Toast.LENGTH_SHORT).show();
+    //         return "";
+    //     }
 
-    }
+    // }
 
 
-        private Intent buildExtra(int status, String message, String rawResponse){
-        Intent data = new Intent();
-        data.putExtra(TXN_STATUS,status);
-        data.putExtra(TXN_MESSAGE,message);
-        data.putExtra(RAW_RESPONSE,rawResponse);
-        return data;
-    }
+    //     private Intent buildExtra(int status, String message, String rawResponse){
+    //     Intent data = new Intent();
+    //     data.putExtra(TXN_STATUS,status);
+    //     data.putExtra(TXN_MESSAGE,message);
+    //     data.putExtra(RAW_RESPONSE,rawResponse);
+    //     return data;
+    // }
 
         ///////////////////////////////////////////////// SHOULD INTERCEPT FUNCTION STARTS HERE /////////////////////////////////////////////
     
@@ -1101,8 +1101,8 @@ public class InAppBrowser extends CordovaPlugin {
 		                    Toast.makeText(this.cordova.getActivity(),"beforePageStarted: Query params exist",Toast.LENGTH_SHORT).show();
 		                    int status = Integer.parseInt(uri.getQueryParameter("TxnStatus"));
 		                    String message = uri.getQueryParameter("TxnMessage");
-		                    String rawResponse = convertQueryToJSON(uri);
-		                    Intent data = buildExtra(status, message, rawResponse);
+		                    //String rawResponse = convertQueryToJSON(uri);
+		                    //Intent data = buildExtra(status, message, rawResponse);
 		                    //listener.onFinish(status, data,triggerReturnUrl);
 
 		                } catch(NumberFormatException e){
@@ -1150,8 +1150,8 @@ public class InAppBrowser extends CordovaPlugin {
 		                    Toast.makeText(this.cordova.getActivity(),"beforePageStarted: Query params exist",Toast.LENGTH_SHORT).show();
 		                    int status = Integer.parseInt(uri.getQueryParameter("TxnStatus"));
 		                    String message = uri.getQueryParameter("TxnMessage");
-		                    String rawResponse = convertQueryToJSON(uri);
-		                    Intent data = buildExtra(status, message, rawResponse);
+		                    //String rawResponse = convertQueryToJSON(uri);
+		                    //Intent data = buildExtra(status, message, rawResponse);
 		                    //listener.onFinish(status, data,triggerReturnUrl);
 
 		                } catch(NumberFormatException e){
