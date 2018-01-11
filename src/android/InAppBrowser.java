bb7b143fd1d8ed6c015ed319e67c4b6bde0258e1
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-11 16:34:02 +0800

    u

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1050,7 +1050,7 @@ public class InAppBrowser extends CordovaPlugin {
             }
 
             else if(url.contains("google")) // if (!triggerReturnUrl && Utils.getURLWithoutParameters(url).contains(merchantReturnURL)) {
-    		{    Toast.makeText(this.cordova.getActivity(),"google",Toast.LENGTH_SHORT).show();
+    		{   LOG.e(LOG_TAG, "google loading...");
     			return false;
     		}
             return false;
