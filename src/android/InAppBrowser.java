d114f332301e73aba3b8cf0aa390bee7777c4cf7
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-13 17:14:17 +0800

    updated

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1124,7 +1124,7 @@ public class InAppBrowser extends CordovaPlugin {
 
             //@TargetApi(Build.VERSION_CODES.N)
             @Override
-            public WebResourceResponse shouldInterceptRequest(WebView webView, WebResourceRequest request) {
+            public boolean WebResourceResponse shouldInterceptRequest(WebView webView, WebResourceRequest request) {
 
                 if(request.getUrl().toString().contains("MerchantReturnURL")) //if (!triggerReturnUrl && Utils.getURLWithoutParameters(request.getUrl().toString()).contains(merchantReturnURL)) {
                 {
