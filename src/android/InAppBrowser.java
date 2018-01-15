7441b600fc508ae2db39af572156870b3d50bbe9
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-15 14:43:55 +0800

    updated

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1072,8 +1072,8 @@ public class InAppBrowser extends CordovaPlugin {
 
         //merchant_return_url
     
-      @SuppressWarnings("deprecation")
-            @Override
+      //@SuppressWarnings("deprecation")
+            //@Override
             public WebResourceResponse shouldInterceptRequest(WebView view, String merchant_return_url) {
 
                 Log.d(TAG, "inside 1st condition - A " + merchant_return_url);
@@ -1143,7 +1143,7 @@ public class InAppBrowser extends CordovaPlugin {
             }
 
             @TargetApi(Build.VERSION_CODES.N)
-            //@Override
+            @Override
             public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
 
                 Log.d(TAG, "inside 2nd condition - A " + request.getUrl().toString());
