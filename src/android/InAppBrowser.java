54dbf3f6df856e7128a7ad40b917194c373e2f73
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-15 21:30:05 +0800

    updated

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -176,7 +176,7 @@ public class InAppBrowser extends CordovaPlugin {
             }
             final String target = t;
             final HashMap<String, Boolean> features = parseFeature(args.optString(2));
-            final String merchant_return_url =  args.optString(3);
+            final String compare_url =  args.optString(3);
 
             LOG.d(LOG_TAG, "target = " + target);
 
