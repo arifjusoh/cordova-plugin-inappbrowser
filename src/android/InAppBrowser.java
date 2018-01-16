152ef4802828b5ba84abdd27ef109eb5b6d86d7a
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-16 16:16:15 +0800

    updated

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -178,8 +178,8 @@ public class InAppBrowser extends CordovaPlugin {
 
             //String url_json = args.getJSONObject(1).toString();
             
-            Toast.makeText(this.cordova.getActivity(),args.getJSONArray(0),Toast.LENGTH_SHORT).show();
-            Toast.makeText(this.cordova.getActivity(),url_json,Toast.LENGTH_SHORT).show();
+            Toast.makeText(this.cordova.getActivity(),args.getJSONArray(0).toString(),Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this.cordova.getActivity(),url_json,Toast.LENGTH_SHORT).show();
 
             final String url = url_united.split("MercURL:")[0];
             compare_url = url_united.split("MercURL:")[1];
